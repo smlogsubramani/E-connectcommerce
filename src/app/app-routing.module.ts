a76cdc1f04a410e9e2ade9cb710bbb55930f8e33
Logasubramani sm <smlogasubramani@gmail.com>
2023-01-04 23:22:54 +0530

    structure created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Manual Imports
 
-const routes: Routes = [];
 
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { BillingComponent } from './billing/billing.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+
+//Route path
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'product',component:ProductComponent},
+  {path:'billing',component:BillingComponent},
+  {path:'feedback',component:FeedbackComponent},
+
+];
+
+
+//common imports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RoutingComponents =[HomeComponent,ProductComponent,BillingComponent,FeedbackComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule ,RoutingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { BillingComponent } from './billing/billing.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponents,
+    NavbarComponent,
+    HomeComponent,
+    ProductComponent,
+    BillingComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
