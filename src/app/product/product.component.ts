baa6f63c3bd81b6eb03b60c78876006b9ee06102
Logasubramani sm <smlogasubramani@gmail.com>
2023-01-07 09:40:30 +0530

    dynamic data update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+  //harcoded array
+  public data = [
+    {"image":"assets/images/carosel.jpg","brand":"sonypql203480","prize":"100"},
+    {"image":"assets/images/post4.jpeg","brand":"Skullcandy","prize":"200"},
+    {"image":"assets/images/carosel.jpg","brand":"boat noise cancelation","prize":"300"},
+    {"image":"assets/images/carosel.jpg","brand":"Raymond shirt","prize":"500"},
+    {"image":"assets/images/carosel.jpg","brand":"Silk saree","prize":"700"}
+    // {"brand":"Silk saree","prize":"700"}
+  ];
 }
